6a130905db716ff8ba11db680860b172666d0c29
masterofheavys <maxsolomon10@gmail.com>
2025-07-07 13:57:09 -0400

    added item data, placeholder texture, crafting recipe

---
+++ src/main/java/net/max/obsidiansword/ObsidianSwordClient.java
@@ -0,0 +1,10 @@
+package net.max.obsidiansword;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ObsidianSwordClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
