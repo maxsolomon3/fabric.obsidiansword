c878de7bc47e7c7962131cb93bd19c4bffa734d5
masterofheavys <maxsolomon10@gmail.com>
2025-07-07 15:25:15 -0400

    Added finalized texture, sword behavior

--- src/main/java/net/max/obsidiansword/item/ModItems.java
+++ src/main/java/net/max/obsidiansword/item/ModItems.java
@@ -4,13 +4,14 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.max.obsidiansword.ObsidianSword;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item OBSIDIAN_SWORD = registerItem("obsidian_sword",new Item(new Item.Settings()));
+    public static final Item OBSIDIAN_SWORD = registerItem("obsidian_sword",new SwordItem(ModToolMaterials.OBSIDIAN,new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.OBSIDIAN,3,2))));
 
     public static void registerModItems()
     {
---
+++ src/main/java/net/max/obsidiansword/item/ModToolMaterials.java
@@ -0,0 +1,63 @@
+package net.max.obsidiansword.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    OBSIDIAN(4,3122,8.0f,10.0f,30,() -> {
+        return Ingredient.ofItems(Items.OBSIDIAN);
+    });
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+
+    private ModToolMaterials(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient)
+    {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return null;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
